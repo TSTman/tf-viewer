def07dfc1f4545b001ecf00e5d788eaed33e3971
morningpee <morningpee2@gmail.com>
2017-06-17 23:31:23 -0500

    scaleFactor is important

--- viewer.user.js
+++ viewer.user.js
@@ -1,6 +1,7 @@
 function scaleToMatch()
 {
     var sizeRatio = innerWidth / 946;
+    var scaleFactor = 234.6 * sizeRatio;
     contentFlash.TSetProperty('/', contentFlashSize.T_WIDTH_SCALE_INDEX, scaleFactor);
     contentFlash.TSetProperty('/', contentFlashSize.T_HEIGHT_SCALE_INDEX, scaleFactor);
     contentFlash.TSetProperty('/', contentFlashSize.T_PAN_X_INDEX, 200.67 - (innerWidth - 573) / 192 * 82 );

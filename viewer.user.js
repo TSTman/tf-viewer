b6401bae03e69fd0df2eb4c6e22071a4d8387006
morningpee <morningpee2@gmail.com>
2017-06-18 00:43:08 -0500

    simplify terms more

--- viewer.user.js
+++ viewer.user.js
@@ -4,7 +4,7 @@ function scaleToMatch()
     var scaleFactor = 234.6 * sizeRatio;
     contentFlash.TSetProperty('/', contentFlashSize.T_WIDTH_SCALE_INDEX, scaleFactor);
     contentFlash.TSetProperty('/', contentFlashSize.T_HEIGHT_SCALE_INDEX, scaleFactor);
-    contentFlash.TSetProperty('/', contentFlashSize.T_PAN_X_INDEX, 200.67 - (innerWidth - 573) / 192 * 82 );
-    contentFlash.TSetProperty('/', contentFlashSize.T_PAN_Y_INDEX, .3 - (innerWidth - 573) / 192 * 95.06 );
+    contentFlash.TSetProperty('/', contentFlashSize.T_PAN_X_INDEX, 445.38875 - innerWidth / 2.341);
+    contentFlash.TSetProperty('/', contentFlashSize.T_PAN_Y_INDEX, 283.9946875 - innerWidth / 2.01977);
     /* contentFlash.style.marginTop = '-50%'; */
 }
